4d11859f1507357bf2b1f61bbb7d25d72379eeec
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-08-28 16:08:04 +0300

    task-2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,99 @@
 import 'flatpickr/dist/flatpickr.min.css';
+import 'notiflix/dist/notiflix-aio-3.2.5.min.js';
 import flatpickr from 'flatpickr';
-const refs = {
-  datetimePickerInput: document.querySelector('[datetime-picker]'),
-  dataStartBtn: document.querySelector('[data-start]'),
-  timer: document.querySelector('.timer'),
+import Notiflix from 'notiflix';
+
+let selecteDateMs;
+let intervId = null;
+let diff = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    saveSelecteDate(selectedDates[0]);
+  },
 };
 
-flatpickr('[datetime-picker]');
+const refs = {
+  datetimePickerInput: document.querySelector('#datetime-picker'),
+  dataStartBtn: document.querySelector('[data-start]'),
+  dataDaysSpan: document.querySelector('[data-days]'),
+  dataHoursSpan: document.querySelector('[data-hours]'),
+  dataMinutesSpan: document.querySelector('[data-minutes]'),
+  dataSecondsSpan: document.querySelector('[data-seconds]'),
+};
+
+refs.dataStartBtn.setAttribute('autocomplete', 'disabled');
+
+flatpickr('#datetime-picker', options);
+
+function saveSelecteDate(date) {
+  selecteDateMs = date.getTime();
+
+  if (selecteDateMs <= Date.now()) {
+    Notiflix.Notify.failure('Please choose a date in the future');
+    return;
+  } else {
+    addDataStartBtnListener();
+    dataStartBtnIsActive();
+  }
+}
+
+function addDataStartBtnListener() {
+  refs.dataStartBtn.addEventListener('click', onDataStartBtnClick);
+}
+
+function dataStartBtnIsActive() {
+  refs.dataStartBtn.removeAttribute('autocomplete', 'disabled');
+}
+
+function onDataStartBtnClick() {
+  dataStartBtnNotActive();
+  if (!intervId) {
+    intervId = setInterval(showTime, 1000);
+  }
+}
+
+function dataStartBtnNotActive() {
+  refs.dataStartBtn.setAttribute('autocomplete', 'disabled');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  return createTimer({ days, hours, minutes, seconds });
+}
+
+function createTimer({ days, hours, minutes, seconds }) {
+  refs.dataDaysSpan.textContent = addLeadingZero(days);
+  refs.dataHoursSpan.textContent = addLeadingZero(hours);
+  refs.dataMinutesSpan.textContent = addLeadingZero(minutes);
+  refs.dataSecondsSpan.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, 0);
+}
+
+function showTime() {
+  diff = selecteDateMs - Date.now();
+  convertMs(diff);
+  if (diff === 0 || diff < 0 || diff < 1000) {
+    clearInterval(intervId);
+  }
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,59 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+import 'notiflix/dist/notiflix-aio-3.2.5.min.js';
+import Notiflix from 'notiflix';
+
+const form = {};
+let position = 0;
+
+const refs = {
+  form: document.querySelector('.form'),
+  formInput: document.querySelectorAll('input'),
+};
+
+refs.form.addEventListener('input', onInputForm);
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onInputForm(e) {
+  e.target.value < 0
+    ? (e.target.value = 0)
+    : (form[e.target.name] = Number(e.target.value));
+
+  return form;
 }
+
+function onSubmitForm(e) {
+  e.preventDefault();
+  for (position = 1; position <= form.amount; position += 1) {
+    createPromise(position, form.delay);
+    console.log(position);
+  }
+  console.log(form);
+}
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(
+          Notiflix.Notify.success(
+            ` Fulfilled promise ${position} in ${delay}ms`
+          )
+        );
+      } else {
+        reject(
+          Notiflix.Notify.failure(` Rejected promise ${position} in ${delay}ms`)
+        );
+      }
+    }, delay);
+  });
+}
+
+// приклад=====================================
+// let result = 20000;
+// function showNumber(num) {
+//   result -= num;
+//   console.log(result);
+//   if (result === 0) {
+//     clearInterval(timeId);
+//   }
+// }
+// let timeId = setInterval(showNumber, 1000, 1000);
